b6679aaecd38a529dd19773c0d30febd817224f2
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-05-29 13:37:56 -0300

    finalizacao interface de registro das movimentacoes - falta API

--- registro.py
+++ registro.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="centered")   # ou "wide"
 margins_css = """
     <style>
         .main > div {
-            padding-top: 10rem;            
+            padding-top: 2rem;            
         }
     </style>
 """
