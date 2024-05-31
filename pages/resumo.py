7c367a4d02eff10d4a00940da5adc34b83e25f1d
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-05-31 15:19:47 -0300

    criacao do menu de navegacao

--- pages/resumo.py
+++ pages/resumo.py
@@ -3,4 +3,11 @@ from menu import menu
 
 menu()
 
+def busca_saldos():
+    tipos = json.loads(json.dumps(requests.get("https://intelliseven.com.br/meteo/finance/saldos").json()))
+
+    for tipo in tipos:
+        lista_tipos_movimento.append(tipo['tipo'])
+    st.session_state['tipos_movimento'] = lista_tipos_movimento
+
 st.title("| Resumo")
