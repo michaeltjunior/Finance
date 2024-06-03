de1886a873df15be2a73860648db68cef5e0fb83
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-06-03 17:26:33 -0300

    formatacao numeros no resumo

--- pages/resumo.py
+++ pages/resumo.py
@@ -1,13 +1,58 @@
 import streamlit as st
+import pandas as pd
 from menu import menu
+import json
+import requests
+
+st.set_page_config(layout="centered")   # ou "wide"
+
+margins_css = """
+    <style>
+        .main > div {
+            padding-top: 1rem;            
+        }
+        
+        .stTextArea textarea {
+            height: 1px;
+        }
+    </style>
+"""
+st.markdown(margins_css, unsafe_allow_html=True)        
 
 menu()
+lista_contas = ["Selecione"]
 
-def busca_saldos():
-    tipos = json.loads(json.dumps(requests.get("https://intelliseven.com.br/meteo/finance/saldos").json()))
+if 'contas' not in st.session_state:
+    st.session_state['contas'] = ''    
 
-    for tipo in tipos:
-        lista_tipos_movimento.append(tipo['tipo'])
-    st.session_state['tipos_movimento'] = lista_tipos_movimento
+def busca_contas():
+    contas = json.loads(json.dumps(requests.get("https://intelliseven.com.br/meteo/finance/contas").json()))
+
+    for conta in contas:
+        lista_contas.append(conta['conta'])
+    st.session_state['contas'] = lista_contas
+
+def mostra_conta(dados):
+    st.table(dados)
+
+def busca_resumo():
+    resumoContas = json.loads(json.dumps(requests.get("https://intelliseven.com.br/meteo/finance/resumo").json()))
+    total = 0.00
+
+    for resumo in resumoContas:        
+        total = total + float(resumo['saldo'])
+
+    df = pd.DataFrame(resumoContas)
+    
+    df.loc[len(df)] = ["Total", total]    
+    df = df.astype({"saldo": float})
+    df["saldo"] = df["saldo"].map("R$ {:,.2f}".format)
+
+    df
 
 st.title("| Resumo")
+
+busca_resumo()
+
+busca_contas()
+listaContas = st.selectbox("Detalhar conta", st.session_state['contas'], key="contaBanco")
