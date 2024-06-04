b07ca7cb50596abd4c9111ab10c809c2e783c03b
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-06-04 12:07:10 -0300

    resumo

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
-from ..menu import menu
+
+import os, sys
+script_path = os.path.realpath(os.path.dirname(__name__))
+os.chdir(script_path)
+sys.path.append("..")
+
+from menu import menu
 #from image_loader import render_image
 
 #render_image(".\pages\logo.jpg")
