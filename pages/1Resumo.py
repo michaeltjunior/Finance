f06632f4df8d88aef2f045725d8699ba384ab152
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-06-04 15:14:27 -0300

    layout resumo

--- pages/1Resumo.py
+++ pages/1Resumo.py
@@ -48,13 +48,14 @@ def busca_resumo():
 
     with st.columns(3)[1]:
         st.markdown("**"+'Saldo total: ' + 'R$ {:,.2f}'.format(total)+"**")
-        #st.write('Saldo total: ' + 'R$ {:,.2f}'.format(total))
 
 def busca_dia():
     st.title("| Para hoje")
 
 st.title("| Resumo")
 st.divider()
+
 busca_resumo()
 st.divider()
+
 busca_dia()
