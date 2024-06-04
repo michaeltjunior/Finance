6466b8e754d12b5182ec03240c8933fa03d6bfd5
michaeltjunior <114235346+michaeltjunior@users.noreply.github.com>
2024-06-04 12:08:51 -0300

    resumo

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-
-import os, sys
-script_path = os.path.realpath(os.path.dirname(__name__))
-os.chdir(script_path)
-sys.path.append("..")
-
 from menu import menu
+
 #from image_loader import render_image
 
 #render_image(".\pages\logo.jpg")
